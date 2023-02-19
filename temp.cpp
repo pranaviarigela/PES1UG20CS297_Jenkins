22c57e9f44b62b38a3371621c3e17be5a44ca543
pranaviarigela <110033410+pranaviarigela@users.noreply.github.com>
2023-02-19 12:23:16 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream> 
+ using namespace std; 
+  
+ int main(void) { 
+   cout << "Hello,This is PRANAVI ARIGELA\n"; 
+   cout << "I am in Task5 and my SRN is PES1UG20CS297\n"; 
+   return 0; 
+ }
