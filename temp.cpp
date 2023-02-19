dbefb34cea3ec634980f4183d36ccda3f7212e4e
pranaviarigela <110033410+pranaviarigela@users.noreply.github.com>
2023-02-19 13:16:05 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,8 +1,8 @@
-#include<iostream> 
- using namespace std; 
-  
- int main(void) { 
-   cout << "Hello,This is PRANAVI ARIGELA\n"; 
-   cout << "I am in Task5 my SRN is PES1UG20CS297\n"; 
-   return 0; 
- }
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World This is MEDA VARUN KUMAR\n";
+  cout << "I am in Task5 my SRN is PES1UG20CS250\n";
+  return 0;
+}
